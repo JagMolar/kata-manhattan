93f40d930f8c63cef1a322017bf2b36385ec45d9
JagMolar <juana.garmue@educa.jcyl.es>
2020-07-18 20:22:24 +0200

    commit formula

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function manhattanDistance(pointA, pointB){
     if(typeof pointA === "number" && typeof pointB === "number" )
         throw new Error("La cagaste con los valores");
     
-        return 7;
+        return Math.abs(pointA[0]- pointB[0]) + Math.abs(pointA[1]- pointB[1]);
 }
 
 module.exports = manhattanDistance;
